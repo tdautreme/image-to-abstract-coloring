5ae1d27dc7eaa5917511897ea0eacfd4e4c41595
Timothée DAUTREME <tim.dautreme.pro@gmail.com>
2023-10-21 23:06:30 +0200

    update remove outline

--- drawconverter.py
+++ drawconverter.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append('./')
 from imagecolorreducer import reduce_image_colors
 from noisesanitizer import sanitize_noise
-from imageoutliner import get_outline_binary_mask
+from imageoutliner import get_outline_binary_mask, make_outline_img, outline_sanitize
 from svgsplitter import split_svg_by_path
 import numpy as np
 import skimage.io as io
@@ -13,32 +13,40 @@ if __name__ == "__main__":
     output_folder_path = sys.argv[2]    
     filename = input_image_path.split("\\")[-1]
     filename_without_extension = filename.split(".")[0]
-    img = io.imread(input_image_path)
+    path_builder = f"{output_folder_path}/{filename_without_extension}_"
+
+    reduced_img_output_path = f"{path_builder}reduced.png"
+    sanitized_reduced_img_output_path = f"{path_builder}reduced_sanitized.png"
+    outline_img_output_path = f"{path_builder}reduced_sanitized_outline.png"
+    outline_img_output_path = f"{path_builder}reduced_sanitized_outline_sanitized.png"
+
 
     # create folder if not exists
     os.makedirs(output_folder_path, exist_ok=True)
 
     # first step is to reduce the number of colors
-    reduced_img = reduce_image_colors(img, color_count=5, iteration=1)
+    img = io.imread(input_image_path)
+    reduced_img = reduce_image_colors(img, color_count=10, iteration=1)
     # write image
-    io.imsave(f"{output_folder_path}/{filename_without_extension}_reduced.png", reduced_img)
+    io.imsave(reduced_img_output_path, reduced_img)
 
     # second step is to remove noise
+    reduced_img = io.imread(reduced_img_output_path)
     sanitized_reduced_img = sanitize_noise(reduced_img, percentage=0.1)
     # write image
-    io.imsave(f"{output_folder_path}/{filename_without_extension}_reduced_sanitized.png", sanitized_reduced_img)
+    io.imsave(sanitized_reduced_img_output_path, sanitized_reduced_img)
 
-    # third step is to get the outline
-    outline_mask = get_outline_binary_mask(sanitized_reduced_img)
-    outline_img = np.array(sanitized_reduced_img)
-    outline_img[outline_mask] = [0, 0, 0]
-    # write image
-    io.imsave(f"{output_folder_path}/{filename_without_extension}_reduced_sanitized_outline.png", outline_img)
+    # # # third step is to get the outline
+    # sanitized_reduced_img = io.imread(sanitized_reduced_img_output_path)
+    # outline_img = make_outline_img(sanitized_reduced_img)
+    # # write image
+    # io.imsave(outline_img_output_path, outline_img)
 
-    # sanitize again after outline
-    sanitized_outline_img = sanitize_noise(outline_img, percentage=0.1)
-    # write image
-    io.imsave(f"{output_folder_path}/{filename_without_extension}_reduced_sanitized_outline_sanitized.png", sanitized_outline_img)
+    # # sanitize outline
+    # outline_img = io.imread(outline_img_output_path)
+    # sanitized_outline_img = outline_sanitize(outline_img)
+    # # write image
+    # io.imsave(outline_img_output_path, sanitized_outline_img)
     exit(0)
 
     '''
--- imageoutliner.py
+++ imageoutliner.py
@@ -27,6 +27,73 @@ def get_outline_binary_mask(img, width=1):
 
     return outline_mask
 
+
+def make_outline_img(img, outline_color=[0,0,0], width=1):
+    for row in range(width, img.shape[0] - width):
+        for col in range(width, img.shape[1] - width):
+            pixel_color = img[row, col]
+            # skip if pixel is already outline color
+            if np.array_equal(pixel_color, outline_color):
+                continue
+
+            # process
+            is_outline_pixel = False
+            for i in range(-width, width + 1):
+                for j in range(-width, width + 1):
+                    if not (i == 0 and j == 0):
+                    # if (i == 0 or j == 0) and not (i == 0 and j == 0):
+                        neighbor_color = img[row + i, col + j]
+                        if not np.array_equal(outline_color, neighbor_color) and not np.array_equal(pixel_color, neighbor_color):
+                            is_outline_pixel = True
+                            break
+                if is_outline_pixel:
+                    break
+            # set pixel to outline color
+            if is_outline_pixel:
+                img[row, col] = outline_color
+    return img
+
+def outline_sanitize(img, outline_color=[0,0,0], width=1):
+    cnt = 0
+    while True:
+        print("outline_sanitize iteration", cnt)
+        work_one_time = False
+        for row in range(width, img.shape[0] - width):
+            for col in range(width, img.shape[1] - width):
+                pixel_color = img[row, col]
+                if not np.array_equal(pixel_color, outline_color):
+                    continue
+                # iam outline pixel
+                # check if outline can be removed
+                is_outline_pixel = False
+                different_colors = []
+                for i in range(-width, width + 1):
+                    for j in range(-width, width + 1):
+                        if not (i == 0 and j == 0):
+                        # if (i == 0 or j == 0) and not (i == 0 and j == 0):
+                            neighbor_color = img[row + i, col + j]
+                            # if neighbor is outline color, skip
+                            if np.array_equal(neighbor_color, outline_color):
+                                continue
+
+                            # check if neighbor color is already in list
+                            is_in_list = False if len(different_colors) == 0 else np.any(np.all(neighbor_color == different_colors, axis=1))
+                            if not is_in_list:
+                                different_colors.append(neighbor_color)
+                                if len(different_colors) > 1:
+                                    is_outline_pixel = True
+                                    break
+                    if is_outline_pixel:
+                        break
+                if len(different_colors) == 1:
+                    # remove outline
+                    img[row, col] = different_colors[0]
+                    work_one_time = True
+        if not work_one_time:
+            break
+        cnt += 1
+    return img
+
 if __name__ == "__main__":
     path = sys.argv[1]
     filename = path.split("\\")[-1]
